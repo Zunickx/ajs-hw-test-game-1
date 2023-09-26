53acbb906ce86201e9c848ba3fc23e5954a7ef83
Zunickx <Zunickx@gmail.com>
2023-09-26 23:31:25 +0300

    Create files test.js, app.js, basic.js, index.html, index.js, babelrc, eslintrc.json, gitignore

---
+++ src/js/app.js
@@ -0,0 +1,3 @@
+import getHPstatus from './basic';
+
+console.log(getHPstatus({ name: 'Маг', health: 14 }));
---
+++ src/js/basic.js
@@ -0,0 +1,9 @@
+// {name: 'Маг', health: 90}
+
+export default function getHPstatus(player) {
+    let statusHP = '';
+    if(player.health >= 50) statusHP = 'healthy';
+    else if(player.health >= 15) statusHP = 'wounded';
+    else statusHP = 'critical';
+    return statusHP;
+}
